b8710c249c90f20e067948f3da45985488974c55
Astolfen <daniil.zarubin.03@mail.ru>
2022-03-08 14:51:00 +0300

    add eighteenth task

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include "string/string_.h"
-#include "string/task/task17.h"
+#include "string/task/task18.h"
 
 int main() {
-    char s1[MAX_STRING_SIZE + 1] = "ssda sfa s";
-    char s2[MAX_STRING_SIZE + 1] = "s a s";
+    char s1[MAX_STRING_SIZE + 1] = "";
+    char s2[MAX_STRING_SIZE + 1] = "s s";
 
-    task17a(s1);
+    task18(s1, s2);
 
     for (int j = 0; j < strlen(s1); ++j) {
         printf("%c", s1[j]);
+    }
+    printf("\n");
+    for (int j = 0; j < strlen(s2); ++j) {
+        printf("%c", s2[j]);
     }
 //    printf("\n%d", task16(s1, s2));
 
