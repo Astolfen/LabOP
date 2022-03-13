801f0f70f6fd1859c0779089198e50c54d16eb1e
Astolfen <daniil.zarubin.03@mail.ru>
2022-03-13 14:10:21 +0300

    dell 5e

--- main.c
+++ main.c
@@ -1,43 +1,5 @@
-#include "string/test_string.h"
-#include "string/task/task1.h"
-#include "string/task/task2.h"
-#include "string/task/task3.h"
-#include "string/task/task4.h"
-#include "string/task/task5.h"
-#include "string/task/task6.h"
-#include "string/task/task8.h"
-#include "string/task/task9.h"
-#include "string/task/task10.h"
-#include "string/task/task11.h"
-#include "string/task/task12.h"
-#include "string/task/task13.h"
-#include "string/task/task14.h"
-#include "string/task/task15.h"
-#include "string/task/task16.h"
-#include "string/task/task17.h"
-#include "string/task/task18.h"
-#include "string/task/task19.h"
 
 int main() {
-    test_string();//OK
-    test_task1();//OK
-    test_task2();//OK
-    test_task3();//OK
-    test_task4();//OK
-    test_task5();//OK
-    test_task6();//OK
-    test_task8();//OK
-    test_task9();//OK
-    test_task10();//OK
-    test_task11();//OK
-    test_task12();//OK
-    test_task13();//OK
-    test_task14();//OK
-    test_task15();//OK
-    test_task16();//OK
-    test_task17();//OK
-    test_task18();//OK
-    test_task19();//OK
 
     return 0;
 }
